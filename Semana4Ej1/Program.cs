448e9ca8af2b60813108b967d369e7ae1349f296
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-26 09:24:44 -0600

    Ejercicio1

--- Semana4Ej1/Program.cs
+++ Semana4Ej1/Program.cs
@@ -6,7 +6,26 @@ namespace Semana4Ej1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array1 = new int[5];
+            array1[2] = 8;
+            array1[0] = 15;
+            for(int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(i + " " + array1[i]);
+            }
+            for (int j = 0; j < array1.Length; j++)
+            {
+                array1[j] = j * 4;
+            }
+            foreach(int row in array1)
+            {
+                Console.WriteLine(row);
+            }
+            string[] paises = new string[5] { "El Salvador", "México", "Chile", "Colombia", "Vemezuela" };
+            foreach (string pais in paises)
+            {
+                Console.WriteLine(pais);
+            }
         }
     }
 }
