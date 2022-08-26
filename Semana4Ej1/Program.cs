539eb3b151846e5edcd7c7a74f0c48d99bbb6749
Alumnos <Alumnos@LABC2PC16>
2022-08-26 08:52:35 -0600

    Agregar archivos de proyecto.

---
+++ Semana4Ej1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semana4Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
